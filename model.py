73b6e9a81d336a541072c7b78448500c633dc817
hakimka <hakimka@hotmail.com>
2017-08-01 22:36:15 -0400

    Add files via upload

---
+++ model.py
@@ -0,0 +1,124 @@
+import cv2
+import csv
+import numpy as np
+import sklearn
+
+
+def generator(samples, batch_size=32):
+	num_samples = len(samples)
+	while 1: # Loop forever so the generator never terminates
+		sklearn.utils.shuffle(samples)
+		for offset in range(0, num_samples, batch_size):
+			batch_samples = samples[offset:offset+batch_size]
+			images=[]
+			measurements=[]
+			for batch_sample in batch_samples:
+				for i in range(3):	
+					source_path = batch_sample[i]
+					filename = source_path.split('\\')[-1]
+					current_path='./data/IMG/'+filename	
+					image = cv2.imread(current_path)	
+					images.append(image)		
+					if (i==0):
+						measurement = float(batch_sample[3]) 
+					else:
+						measurement = float(batch_sample[3]) + 0.15*float((-1**(i+1)))
+					measurements.append(measurement)
+			aug_images=[]
+			aug_measurements=[]	
+			for image, measurement in zip (images, measurements):
+				aug_images.append(image)
+				aug_measurements.append(measurement)
+				aug_images.append(cv2.flip(image,1))
+				aug_measurements.append(measurement*-1.0)
+	
+	
+			X_train = np.array(aug_images)
+			y_train = np.array(aug_measurements)
+			
+			#yield (X_train, y_train)            
+			yield sklearn.utils.shuffle(X_train, y_train)
+
+
+
+samples  = []
+with open('./data/driving_log.csv') as csvfile:
+	reader = csv.reader(csvfile)
+	for line in reader:
+		samples .append(line)
+
+		
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
+
+	
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten, Lambda
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, Cropping2D
+
+# convolution kernel size
+nb_conv = 3
+
+# number of convolutional filters to use
+nb_filters = 32
+
+nb_pool = 2 
+
+model = Sequential()
+
+model.add(Lambda(lambda x: x/255.0-0.5, input_shape =(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
+
+model.add(Convolution2D(32, 3, 3, activation='relu'))
+model.add(Convolution2D(32, 3, 3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+#2
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+#3
+model.add(Convolution2D(10, nb_conv, nb_conv))							
+model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+##4
+#model.add(Convolution2D(16, nb_conv, nb_conv))
+#model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+#model.add(Activation('relu'))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(32))
+model.add(Activation('relu'))
+
+
+model.add(Dense(1))
+
+
+model.compile(loss='mse', optimizer='adam')
+model.fit_generator(train_generator, samples_per_epoch= len(train_samples), 
+                           validation_data=validation_generator, 
+                           nb_val_samples=len(validation_samples), nb_epoch=10)
+
+#model.fit(X_train,y_train, validation_split=0.3, shuffle=True, nb_epoch=15)
+
+model.save('model.h5')
